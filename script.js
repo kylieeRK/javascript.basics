58a3ac63ec148ec85733f890c1292f4f800b4112
kylie Kertley <kyliek111407@gmail.com>
2023-11-28 17:05:35 -0500

    first

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("hello world");
+
+//weird scope - dont use 
+// var x = 5;
+// regular old variable
+let y=6;
+y=10; // you can change the value 
+
+const z = 7; // this is a constant. Cant be changed 
+// z=18; // this throws and error.
+
+console.log(y,z);
+console.log(y+z);
+console.log(y-z);
+console.log(y*z);
+console.log(y/z);
+console.log(y%z); // this does divison but returns the remainder
+console.log(y**z); // exponents
+
+let fname="kylie ";
+lname="kertley";
+console.log(fname + " " + lname);
+
+console.log(`${ fname }  ${ lname }`);
+console.log(`${ fname} ${ lname}`);
+console.log( lname + " " +  6);
+
+
+let user = "kylie";
+let pass = "foo";
+
+if (user == "kylie") {
+    console.log('you are ' + user + ` and your pass is ` + pass);
+} else {
+    console.error("DENIED");
+}
+
+const license = 18;
+const jrop = 16;
+const rental = 25;
+
+let myAge = 46;
+
+function checkAge() {
+
+}
